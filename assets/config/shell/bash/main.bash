1fb32306f4541cc46c9c1be53f8d391e736abdef
Nullifi3r <tools.e@outlook.com>
2026-09-20 22:57:36 +0300

    feat(tools): Add Flyline Library plugin for bash

--- assets/config/shell/bash/main.bash
+++ assets/config/shell/bash/main.bash
@@ -15,7 +15,9 @@ if [ -f /etc/bashrc ]; then
     . /etc/bashrc
 fi
 ################################################
-
+if getconf GNU_LIBC_VERSION >/dev/null 2>&1; then
+    enable flyline 2>/dev/null || enable -f "$UNISHELL_SESSION_RUNTIME_DIR/bin/libflyline.so.1.8.0" flyline
+fi
 ################################################
 ## VARIABLES
 ################################################
